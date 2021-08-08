4e1809262a11837fcfa5522cc353151aebb8ef85
omatsudayo <matsuri.motoki.5c@stu.hosei.ac.jp>
2021-08-08 18:41:05 +0900

    HelloWorld.vueを削除

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+```html
+<template>
+  <div id="app">
+    <div class="hello-wrold">
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <div class="hello-wrold__message">
+        {{ message }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: function () {
+    return {
+      message: "WebExpert Course Vue Template",
+    }
+  },
+  methods: {},
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
+
+<style scoped>
+.hello-wrold {
+  margin-top: 60px;
+  text-align: center;
+}
+
+.hello-wrold .hello-world__message {
+  color: #2c3e50;
+}
+</style>
+```
