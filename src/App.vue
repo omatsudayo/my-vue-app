cd8dcd7cd8358447051cf79eb80705bf71eb892e
omatsudayo <matsuri.motoki.5c@stu.hosei.ac.jp>
2021-08-08 18:46:34 +0900

    TodoListを作成した

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,17 @@
         {{ message }}
       </div>
     </div>
+    **<TodoList />**
   </div>
 </template>
 
 <script>
+**import TodoList from "@/components/TodoList.vue"**
 export default {
   name: "App",
+  **components: {
+    TodoList
+  },**
   data: function () {
     return {
       message: "WebExpert Course Vue Template",
